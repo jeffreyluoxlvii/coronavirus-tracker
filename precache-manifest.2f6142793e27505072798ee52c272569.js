8761a8dafcc0510fcd566f64b3894cc99ab61358
Jeffrey Luo <jl834@duke.edu>
2020-04-28 21:56:21 -0700

    Updates

---
+++ precache-manifest.2f6142793e27505072798ee52c272569.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6efd4390165e5c712a902c4548b3219c",
+    "url": "/coronavirus-tracker/index.html"
+  },
+  {
+    "revision": "a606856b9e8169a2e23f",
+    "url": "/coronavirus-tracker/static/css/main.53c5754c.chunk.css"
+  },
+  {
+    "revision": "83f2b2b808ffe8ab5ab5",
+    "url": "/coronavirus-tracker/static/js/2.d0806cab.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/coronavirus-tracker/static/js/2.d0806cab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a606856b9e8169a2e23f",
+    "url": "/coronavirus-tracker/static/js/main.70902501.chunk.js"
+  },
+  {
+    "revision": "fb38f4fc8125ca09001d",
+    "url": "/coronavirus-tracker/static/js/runtime-main.5a822610.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/coronavirus-tracker/static/media/image.d7265326.png"
+  }
+]);
